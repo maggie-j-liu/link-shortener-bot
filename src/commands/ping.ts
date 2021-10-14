aae6e02fa5539e076dcd1587252c1b9ea72a5d07
maggie-j-liu <maggie.j.liu@gmail.com>
2021-10-14 12:45:17 -0700

    permissions

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -3,7 +3,8 @@ import { CommandInteraction } from "discord.js";
 const info = {
   data: new SlashCommandBuilder()
     .setName("ping")
-    .setDescription("Replies with pong!"),
+    .setDescription("Replies with pong!")
+    .setDefaultPermission(false),
   execute: async (interaction: CommandInteraction) => {
     await interaction.reply("pong");
   },
--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,12 +1,14 @@
-import { SlashCommandBuilder } from "@discordjs/builders";
 import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v9";
 import dotenv from "dotenv";
 import fs from "fs";
 
 dotenv.config();
+const clientId = process.env.CLIENT_ID as string;
+const guildId = process.env.GUILD_ID as string;
+const linkAdminRoleId = process.env.LINK_ADMIN_ROLE_ID as string;
 
-const commands = [];
+const commands: string[] = [];
 
 const commandFiles = fs
   .readdirSync(`${__dirname}/commands`)
@@ -21,13 +23,54 @@ const rest = new REST({ version: "9" }).setToken(
   process.env.DISCORD_TOKEN as string
 );
 
-rest
-  .put(
-    Routes.applicationGuildCommands(
-      process.env.CLIENT_ID as string,
-      process.env.GUILD_ID as string
-    ),
+interface Command {
+  id: string;
+  application_id: string;
+  name: string;
+  description: string;
+  version: string;
+  default_permission: boolean;
+  type: number;
+  guild_id: string;
+}
+
+// type: ROLE = 1, USER = 2
+interface Permission {
+  id: string;
+  type: number;
+  permission: boolean;
+}
+interface FullPermission {
+  id: string;
+  permissions: Permission[];
+}
+
+(async () => {
+  const response: Command[] = (await rest.put(
+    Routes.applicationGuildCommands(clientId, guildId),
     { body: commands }
-  )
-  .then(() => console.log("Successfully registered application commands."))
-  .catch(console.error);
+  )) as Command[];
+  console.log("Successfully registered application commands.");
+  // console.log(response);
+  const restrictedCommands = response.filter(
+    (cmd) => cmd.default_permission === false
+  );
+  const fullPermissions: FullPermission[] = [];
+  for (const command of restrictedCommands) {
+    fullPermissions.push({
+      id: command.id,
+      permissions: [
+        {
+          id: linkAdminRoleId,
+          type: 1,
+          permission: true,
+        },
+      ],
+    });
+  }
+  await rest.put(
+    Routes.guildApplicationCommandsPermissions(clientId, guildId),
+    { body: fullPermissions }
+  );
+  console.log("Successfully updated permissions");
+})();
